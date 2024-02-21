cbb8d0a336eaf0df241e0c2a2db4c5949b859f3a
Rossy <Mayrenapine30@outlook.es>
2024-02-21 08:44:46 -0600

    Conversores Unidades de palsila 2

--- Calculadora/app/src/main/java/com/example/calculadora/MainActivity.java
+++ Calculadora/app/src/main/java/com/example/calculadora/MainActivity.java
@@ -28,25 +28,20 @@ public class MainActivity extends AppCompatActivity {
         tbh = findViewById(R.id.tbhConversores);
         tbh.setup();
 
-        tbh.addTab(tbh.newTabSpec("LONGITUD").setContent(R.id.tabLongitud).setIndicator("LONGITUD", null));
+        tbh.addTab(tbh.newTabSpec("Área").setContent(R.id.tabÁrea).setIndicator("Área", null));
         tbh.addTab(tbh.newTabSpec("ALMACENAMIENTO").setContent(R.id.tabAlmacenamiento).setIndicator("ALMACENAMIENTO", null));
-        tbh.addTab(tbh.newTabSpec("MONEDAS").setContent(R.id.tabMonedas).setIndicator("MONEDAS",null));
-        tbh.addTab(tbh.newTabSpec("Masa").setContent(R.id.tabMasa).setIndicator("Masa",null));
-        tbh.addTab(tbh.newTabSpec("Tiempo").setContent(R.id.tabTiempo).setIndicator("Tiempo",null));
-        tbh.addTab(tbh.newTabSpec("Transferencia_de_datos").setContent(R.id.tabTransferencia_de_datos).setIndicator("Transferencia_de_datos",null));
-        tbh.addTab(tbh.newTabSpec("Volumen").setContent(R.id.tabVolumen).setIndicator("Volumen",null));
-        btn = findViewById(R.id.btnLongitudConvertir);
+        btn = findViewById(R.id.btnÁreaConvertir);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try{
-                spn = findViewById(R.id.spnLongitudDe);
+                spn = findViewById(R.id.spnÁreaDe);
                 int de = spn.getSelectedItemPosition();
 
-                spn = findViewById(R.id.spnLongitudA);
+                spn = findViewById(R.id.spnÁreaA);
                 int a = spn.getSelectedItemPosition();
 
-                tempVal = findViewById(R.id.txtLongitudCantidad);
+                tempVal = findViewById(R.id.txtÁreaCantidad);
                 double cantidad = Double.parseDouble(tempVal.getText().toString());
 
                 double resp = objConversor.convertir(0, de, a, cantidad);
@@ -75,114 +70,12 @@ public class MainActivity extends AppCompatActivity {
                 }catch(Exception e){}
             }
         });
-        btn = findViewById(R.id.btnMonedasConvertir);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try{
-                spn = findViewById(R.id.spnMonedasDe);
-                int de = spn.getSelectedItemPosition();
-
-                spn = findViewById(R.id.spnMonedasA);
-                int a = spn.getSelectedItemPosition();
-
-                tempVal = findViewById(R.id.txtMonedasCantidad);
-                double cantidad = Double.parseDouble(tempVal.getText().toString());
-
-                double resp = objConversor.convertir(2, de, a, cantidad);
-                Toast.makeText(getApplicationContext(),"Respuesta: "+
-                        resp, Toast.LENGTH_LONG).show();
-                }catch(Exception e){}
-            }
-        });
-        btn = findViewById(R.id.btnMasaConvertir);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try{
-                spn = findViewById(R.id.spnMasaDe);
-                int de = spn.getSelectedItemPosition();
-
-                spn = findViewById(R.id.spnMasaA);
-                int a = spn.getSelectedItemPosition();
-
-                tempVal = findViewById(R.id.txtMasaCantidad);
-                double cantidad = Double.parseDouble(tempVal.getText().toString());
-
-                double resp = objConversor.convertir(3, de, a, cantidad);
-                Toast.makeText(getApplicationContext(),"Respuesta: "+
-                        resp, Toast.LENGTH_LONG).show();
-                }catch(Exception e){}
-            }
-        });
-        btn = findViewById(R.id.btnTiempoConvertir);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try{
-                spn = findViewById(R.id.spnTiempoDe);
-                int de = spn.getSelectedItemPosition();
-
-                spn = findViewById(R.id.spnTiempoA);
-                int a = spn.getSelectedItemPosition();
-
-                tempVal = findViewById(R.id.txtTiempoCantidad);
-                double cantidad = Double.parseDouble(tempVal.getText().toString());
-
-                double resp = objConversor.convertir(4, de, a, cantidad);
-                Toast.makeText(getApplicationContext(),"Respuesta: "+
-                        resp, Toast.LENGTH_LONG).show();
-                }catch(Exception e){}
-            }
-        });
-        btn = findViewById(R.id.btnTransferencia_de_datosConvertir);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                try{
-                    spn = findViewById(R.id.spnTransferencia_de_datosDe);
-                    int de = spn.getSelectedItemPosition();
-
-                    spn = findViewById(R.id.spnTransferencia_de_datosA);
-                    int a = spn.getSelectedItemPosition();
-
-                    tempVal = findViewById(R.id.txtTransferencia_de_datosCantidad);
-                    double cantidad = Double.parseDouble(tempVal.getText().toString());
-
-                    double resp = objConversor.convertir(5, de, a, cantidad);
-                    Toast.makeText(getApplicationContext(),"Respuesta: "+
-                            resp, Toast.LENGTH_LONG).show();
-                }catch(Exception e){}
-            }
-        });
-        btn = findViewById(R.id.btnVolumenConvertir);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                try{
-                    spn = findViewById(R.id.spnVolumenDe);
-                    int de = spn.getSelectedItemPosition();
-
-                    spn = findViewById(R.id.spnVolumenA);
-                    int a = spn.getSelectedItemPosition();
-
-                    tempVal = findViewById(R.id.txtVolumenCantidad);
-                    double cantidad = Double.parseDouble(tempVal.getText().toString());
-
-                    double resp = objConversor.convertir(6, de, a, cantidad);
-                    Toast.makeText(getApplicationContext(),"Respuesta: "+
-                            resp, Toast.LENGTH_LONG).show();
-                }catch(Exception e){}
-            }
-        });
     }
 }
 class conversores{
     double[][] valores = {
-            //LONGITUD
-            {1, 100, 39.3701, 3.280841666667, 1.193, 1.0936138888889999077, 0.001, 0.000621371, 0.001, 0.000001, 0.000000001},
+            //Área
+            {1, 0.6988,7.52, 0.0001, 0.0001726,1.296,628.8},
 
             //Almacenamiento
             {1, 8, 1000 * 8, Math.pow(1000, 2) * 8, Math.pow(1000, 3) * 8, Math.pow(1000, 4) * 8, Math.pow(1000, 5) * 8, Math.pow(1000, 6) * 8, Math.pow(1000, 7) * 8,
